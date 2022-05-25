5ddeb40fa096accc7317d67dcef4432d048c3ffb
laurencetuchin <laurencetuchin@gmail.com>
2022-05-25 11:58:13 +1000

    Add IV and constructors to User

--- src/main/java/com/laurencetuchin/realestateapp/entities/User.java
+++ src/main/java/com/laurencetuchin/realestateapp/entities/User.java
@@ -5,5 +5,21 @@ import javax.persistence.Entity;
 @Entity
 public class User {
     private Integer id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+
+    public User() {
+    }
+
+    public User(String username, String firstName, String lastName, String phoneNumber) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
 
 }
