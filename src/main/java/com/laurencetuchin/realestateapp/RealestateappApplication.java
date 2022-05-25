349d28e044eb85866510f7a0192a569a27a997f5
laurencetuchin <laurencetuchin@gmail.com>
2022-05-25 23:01:38 +1000

    Fix H2 console

--- src/main/java/com/laurencetuchin/realestateapp/RealestateappApplication.java
+++ src/main/java/com/laurencetuchin/realestateapp/RealestateappApplication.java
@@ -2,8 +2,11 @@ package com.laurencetuchin.realestateapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class RealestateappApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/laurencetuchin/realestateapp/repositories/RoleRepository.java
+++ src/main/java/com/laurencetuchin/realestateapp/repositories/RoleRepository.java
@@ -1,10 +1,10 @@
 package com.laurencetuchin.realestateapp.repositories;
 
-import com.laurencetuchin.realestateapp.entities.User;
+import com.laurencetuchin.realestateapp.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface RoleRepository extends JpaRepository<Role, Integer> {
 
 }
