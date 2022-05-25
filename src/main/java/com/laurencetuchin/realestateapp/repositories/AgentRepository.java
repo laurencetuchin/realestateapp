3fa2cd635a56aea7651b58c236dabc4dfc41d36e
laurencetuchin <laurencetuchin@gmail.com>
2022-05-25 23:04:00 +1000

    Add repositories

--- src/main/java/com/laurencetuchin/realestateapp/repositories/AgentRepository.java
+++ src/main/java/com/laurencetuchin/realestateapp/repositories/AgentRepository.java
@@ -1,7 +1,10 @@
 package com.laurencetuchin.realestateapp.repositories;
 
+import com.laurencetuchin.realestateapp.entities.Agent;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AgentRepository {
+public interface AgentRepository extends JpaRepository<Agent, Integer> {
+
 }
--- src/main/java/com/laurencetuchin/realestateapp/repositories/PropertyRepository.java
+++ src/main/java/com/laurencetuchin/realestateapp/repositories/PropertyRepository.java
@@ -1,7 +1,9 @@
 package com.laurencetuchin.realestateapp.repositories;
 
+import com.laurencetuchin.realestateapp.entities.Property;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PropertyRepository {
+public interface PropertyRepository extends JpaRepository<Property, Integer> {
 }
