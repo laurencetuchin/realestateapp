bf271dc27d4e6053f7d31aa0786290fce2bf8a8a
laurencetuchin <laurencetuchin@gmail.com>
2022-05-26 15:02:48 +1000

    Add user create controller

--- src/main/java/com/laurencetuchin/realestateapp/controllers/UserController.java
+++ src/main/java/com/laurencetuchin/realestateapp/controllers/UserController.java
@@ -1,8 +1,27 @@
 package com.laurencetuchin.realestateapp.controllers;
 
+import com.laurencetuchin.realestateapp.entities.User;
+import com.laurencetuchin.realestateapp.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/adduser")
+    public User addUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PostMapping("/addmanyuser")
+    public List<User> addUserList(@RequestBody List<User> user) {
+        return userService.createUserList(user);
+    }
 }
