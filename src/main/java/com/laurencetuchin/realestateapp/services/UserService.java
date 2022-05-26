c773101d91df4d2afe80f4004c8fc2c969f910ad
laurencetuchin <laurencetuchin@gmail.com>
2022-05-26 10:53:46 +1000

    Add getuserbyid

--- src/main/java/com/laurencetuchin/realestateapp/services/UserService.java
+++ src/main/java/com/laurencetuchin/realestateapp/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -19,4 +20,10 @@ public class UserService {
     public List<User> createUserList(List<User> users) {
         return userRepository.saveAll(users);
     }
+
+    public Optional<User> getUserById(Integer id) {
+        return userRepository.findById(id);
+    }
+
+
 }
