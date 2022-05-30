8e367960e55537f3f56147464c07d4780af7e290
laurencetuchin <laurencetuchin@gmail.com>
2022-05-30 17:22:40 +1000

    Update agent create controller

--- src/main/java/com/laurencetuchin/realestateapp/controllers/AgentController.java
+++ src/main/java/com/laurencetuchin/realestateapp/controllers/AgentController.java
@@ -1,7 +1,21 @@
 package com.laurencetuchin.realestateapp.controllers;
 
+import com.laurencetuchin.realestateapp.entities.Agent;
+import com.laurencetuchin.realestateapp.services.AgentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class AgentController {
+
+    @Autowired
+    private AgentService agentService;
+
+    @PostMapping("/agent/new")
+    public Agent createAgent(Agent agent) {
+        return agentService.createAgent(agent);
+    }
 }
